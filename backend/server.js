48345332900460a79fe6b9b99df9bd14d2abff9e
Dustysea <49942226+slothrobot@users.noreply.github.com>
2022-11-19 02:20:08 +0900

    prepare for deployment 2

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ if(process.env.NODE_ENV === 'production'){
     app.use(express.static('frontend/build'));
     // index.html for all page routes
     app.get('*', (req, res) =>{
-        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+        res.sendFile(path.resolve(__dirname, '../frontend', 'build', 'index.html'));
     });
 }
 
